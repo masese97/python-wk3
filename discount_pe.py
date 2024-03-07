0fe67ac134384916475dd89a3941d716fe99a7dc
masese97 <153599807+masese97@users.noreply.github.com>
2024-03-07 16:09:57 +0300

    Add files via upload

---
+++ discount_pe.py
@@ -0,0 +1,19 @@
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        discounted_price = price - (price * (discount_percent / 100))
+        return discounted_price
+    else:
+        return price
+
+# Prompt the user to enter original price and discount percentage
+original_price = float(input("Enter the original price of the item: "))
+discount_percent = float(input("Enter the discount percentage: "))
+
+# Calculate the final price after applying the discount
+final_price = calculate_discount(original_price, discount_percent)
+
+# Print the final price
+if final_price != original_price:
+    print("Final price after applying a discount: {:.2f}".format(final_price))
+else:
+    print("No discount applied. Final price: {:.2f}".format(final_price))
